34189b96f3ae9c71726ba0f5004560ac2a7b2749
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2019-06-17 23:23:23 -0300

    finish lex method

--- lexer/src/main/java/lexer/Lexer.java
+++ lexer/src/main/java/lexer/Lexer.java
@@ -1,13 +1,62 @@
 package lexer;
 
+import lexer.lexerStates.DefaultState;
+import lexer.lexerStates.ErrorState;
+import lexer.lexerStates.LexerState;
+import lexer.lexerStates.StateConsumeResult;
+import lexer.tokens.Token;
+import lexer.tokens.TokenType;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Lexer {
-    private List<Token> tokens;
-    private List<LexError> errors;
+    private LexerInfo lexerInfo;
+    private LexerState currentState = new DefaultState();
+    private List<Token> tokens = new ArrayList<>();
+    private List<LexError> errors = new ArrayList<>();
+    private int index = 0;
+    private StateConsumeResult result;
+
+    public Lexer(Map<String, TokenType> tokenTable) {
+        this.lexerInfo = new LexerInfo(tokenTable);
+    }
 
     LexResult lex(String code) {
-        return new LexResult(new ArrayList<>(), new ArrayList<>());
+        result = null;
+        for (index = 0; index < code.length(); incrementIndex()) {
+            char c = code.charAt(index);
+            result = currentState.consume(c, lexerInfo);
+            lexByType();
+        }
+        return new LexResult(tokens, errors, errors.isEmpty());
+    }
+
+    private void lexByType() {
+        switch (result.getType()) {
+            case ACCEPTED:
+                Token token = result.getToken();
+                if (token.getType() == TokenType.UNKNOWN) errors.add(LexError.unexpectedToken(token));
+                else tokens.add(token);
+                currentState = new DefaultState();
+                break;
+            case TRANSITION:
+                currentState = result.getNextState();
+                break;
+            case NEW_LINE:
+                lexerInfo.nextLine();
+                break;
+            case ERROR:
+                currentState = new ErrorState();
+                break;
+        }
+    }
+
+    private void incrementIndex() {
+        if (result.isConsumed()) {
+            lexerInfo.nextColumn();
+            index++;
+        }
     }
 }
